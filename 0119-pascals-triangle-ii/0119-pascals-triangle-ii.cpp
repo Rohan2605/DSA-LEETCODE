7e586dcad29ab11159fc470d24c502a873597eec
Rohan Kandpal <83447838+Rohan2605@users.noreply.github.com>
2026-05-21 18:09:49 +0530

    Time: 0 ms (100%), Space: 9.1 MB (22.27%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> getRow(int n) {
+        vector<vector<int>> ans;
+
+        for(int i = 0; i <n+1; i++)
+        {
+            vector<int> row(i+1, 1);
+            for(int j = 1; j < i; j++)
+            {
+                row[j] = ans[i-1][j-1] + ans[i-1][j];
+               
+            }
+            ans.push_back(row);
+            if(i == n) return row;
+        }
+        return {};
+    }
+};
