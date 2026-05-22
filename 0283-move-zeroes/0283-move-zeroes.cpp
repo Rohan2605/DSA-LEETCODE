114260b64f0006b8139e6996c9be430c9027eace
Rohan Kandpal <83447838+Rohan2605@users.noreply.github.com>
2026-05-22 15:19:19 +0530

    Time: 0 ms (100%), Space: 23.9 MB (54.48%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,18 +3,11 @@ public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
         int i=0;
-        int j=1;
 
-        while(j<n){
-            if(nums[i]==0 && nums[j]!=0){
+        for(int j=0; j<n; j++){
+            if(nums[j]!=0){
                 swap(nums[i], nums[j]);
-                i++; j++;
-            }
-            else if(nums[i]==0 && nums[j]==0){
-                j++;
-            }
-            else {
-                j++; i++;
+                i++;
             }
         }
     }
