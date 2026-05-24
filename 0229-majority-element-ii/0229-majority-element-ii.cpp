f17910faa544416e4350a41a92339201dff01164
Rohan Kandpal <83447838+Rohan2605@users.noreply.github.com>
2026-05-24 13:50:07 +0530

    Time: 0 ms (100%), Space: 23.7 MB (44.78%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,18 +1,40 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int, int> mp;
-        vector<int> v;
+        int cnt1=0, cnt2=0;
+        int cand1=0, cand2=0;
+        int n = nums.size();
+        vector<int> result;
 
-        for(int x :nums){
-            mp[x]++;
+        for(int i=0; i<n; i++){
+            if(cnt1==0 && nums[i] != cand2){
+                cnt1++;
+                cand1 = nums[i];
+            }
+            else if(cnt2==0 && nums[i] != cand1){
+                cnt2++;
+                cand2 = nums[i];
+            }
+            else if(cand1==nums[i]) cnt1++;
+            else if(cand2==nums[i]) cnt2++;
+            else {
+                cnt1--;
+                cnt2--;
+            }
         }
 
-        int n = nums.size()/3;
+        int threshold = n/3;
 
-        for(auto x: mp){
-            if(x.second > n) v.push_back(x.first);
+        cnt1=0;
+        cnt2=0;
+        for(int x: nums){
+            if(x == cand1) cnt1++;
+            else if(x == cand2) cnt2++;
         }
-        return v;
+
+        if(cnt1>threshold) result.push_back(cand1);
+        if(cnt2>threshold) result.push_back(cand2);
+
+        return result;
     }
 };
