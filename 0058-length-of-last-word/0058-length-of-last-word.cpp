5e7848531a5efb349271e6a7b6f5468a9b7677f4
Rohan Kandpal <83447838+Rohan2605@users.noreply.github.com>
2026-05-30 21:29:57 +0530

    Time: 0 ms (100%), Space: 8.8 MB (66.98%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int start = s.find_first_not_of(' ');
-        int end = s.find_last_not_of(' ');
-        string x = s.substr(start, end-start+1);
-        cout<<s<<endl;
+        int i = s.size()-1;
+
+        while(i>=0 && s[i]==' '){ i--; }
         int cnt = 0;
-        for(int i=x.size()-1; i>=0; i--){
-            if(x[i] == ' ') break;
-            cnt++;
-        }
+        while(i>=0 && s[i]!=' '){ cnt++; i--;}
+
         return cnt;
     }
 };
