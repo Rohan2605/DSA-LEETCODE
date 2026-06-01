2ce1f208b2fc906267c19832eeeaf7ebaed4f720
Rohan Kandpal <83447838+Rohan2605@users.noreply.github.com>
2026-06-02 00:48:11 +0530

    Time: 0 ms (100%), Space: 8.5 MB (92.41%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
     double myPow(double x, long n) {
-        if(x==1) return 1;
-        if(n==0) return 1;
-        if(n<0){
-            return myPow(1/x, -n);
+        long long N = n;
+
+        if(N<0){
+            x = 1/x;
+            N = - N;
         }
-        if(n%2 == 0){
-            return myPow(x*x, n/2);
+
+        if(n == 0 || x == 1) return 1;
+        
+        double ans = 1;
+        while(N>0){
+            if(N%2 == 1){
+                ans *= x;
+            }
+            x *= x;
+            N = N/2;
         }
-        else{
-            return x * myPow(x*x, n/2);
-        }
+
+        return ans;
     }
 };
