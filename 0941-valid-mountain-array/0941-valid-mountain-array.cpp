28cd04c28ff2b91e8e48d06126de6e1ff7d66129
Rohan Kandpal <83447838+Rohan2605@users.noreply.github.com>
2026-06-03 20:02:32 +0530

    Time: 21 ms (42.2%), Space: 26.2 MB (74.7%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.cpp
+++ 0941-valid-mountain-array/0941-valid-mountain-array.cpp
@@ -4,13 +4,16 @@ public:
         int n = arr.size();
         int i=0;
 
+        // walk up the mountain
         while(i+1<n && arr[i]<arr[i+1]) i++;
 
+        // the top is not start or end as then it cannot be a mountain
         if(i==0 || i==n-1) return false;
 
+        // walk down the mountain
         while(i+1<n && arr[i]>arr[i+1]) i++;
 
+        // if i is equal to n-1 then a mountain array else not
         return i==n-1;
-        
     }
 };
