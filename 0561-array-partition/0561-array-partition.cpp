96d63d29b7438e7286ccf307c8de58069c120c1d
Rohan Kandpal <83447838+Rohan2605@users.noreply.github.com>
2026-06-03 14:22:30 +0530

    Time: 31 ms (5.55%), Space: 33.1 MB (4.44%) - LeetHub

--- 0561-array-partition/0561-array-partition.cpp
+++ 0561-array-partition/0561-array-partition.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int arrayPairSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        priority_queue<int, vector<int>, greater<int>> pq(nums.begin(), nums.end());
         int ans = 0;
-
-        for(int i=0; i<nums.size(); i+=2){
-            ans += nums[i];
-        }   
+        while(pq.size()>0){
+            ans += pq.top();
+            pq.pop();
+            pq.pop();
+        }
         return ans;
     }
 };
