e9107f7bc3c528c0268d789e9c2ab405a44d1d34
Rohan Kandpal <83447838+Rohan2605@users.noreply.github.com>
2026-06-03 14:03:39 +0530

    Time: 9 ms (62.58%), Space: 32 MB (84.96%) - LeetHub

--- 0561-array-partition/0561-array-partition.cpp
+++ 0561-array-partition/0561-array-partition.cpp
@@ -5,7 +5,7 @@ public:
         int ans = 0;
 
         for(int i=0; i<nums.size(); i+=2){
-            ans += min(nums[i], nums[i+1]);
+            ans += nums[i];
         }   
         return ans;
     }
