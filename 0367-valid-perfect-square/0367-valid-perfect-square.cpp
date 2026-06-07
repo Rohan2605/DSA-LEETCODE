e534bfbaefa853a1bc4fbc79be8e8e4de1578135
Rohan Kandpal <83447838+Rohan2605@users.noreply.github.com>
2026-06-07 23:51:12 +0530

    Time: 0 ms (100%), Space: 7.9 MB (16.86%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.cpp
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -1,7 +1,14 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-        auto x = sqrt(num);
-        return (floor(x)==x)?true: false;
+        long long start = 1, end = num;
+        long long mid;
+        while(end>=start){
+            mid = (start + end)/2;
+            if(mid*mid == num) return true;
+            else if(mid*mid > num) end = mid-1;
+            else start = mid+1;
+        }
+        return false;
     }
 };
