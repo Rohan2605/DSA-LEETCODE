86ad139169ccd704397cc167bef91190b26be5e9
Rohan Kandpal <83447838+Rohan2605@users.noreply.github.com>
2026-06-17 10:01:05 +0530

    Time: 0 ms (100%), Space: 14.8 MB (54.55%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,16 +1,33 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> mp;
+        // unordered_map<int, int> mp;
 
-        for(int i=0; i<nums.size(); i++){
-            mp[nums[i]] = i;
-        }
+        // for(int i=0; i<nums.size(); i++){
+        //     mp[nums[i]] = i;
+        // }
 
-        for(int i=0; i<nums.size(); i++){
-            int complement = target - nums[i];
-            if(mp.count(complement )  && mp[complement]!=i) return {i, mp[complement]};
-        }
-        return {};
+        // for(int i=0; i<nums.size(); i++){
+        //     int complement = target - nums[i];
+        //     if(mp.count(complement )  && mp[complement]!=i) return {i, mp[complement]};
+        // }
+        // return {};
+
+       vector<pair<int, int>> arr;
+
+       for(int i=0; i<nums.size(); i++){
+            arr.push_back({nums[i], i});
+       }
+
+       sort(arr.begin(), arr.end());
+
+       int left=0, right=arr.size()-1;
+       while(left<right){
+            int sum = arr[left].first + arr[right].first;
+            if(sum==target) return {arr[left].second, arr[right].second};
+            else if(sum>target) right--;
+            else left++;
+       }
+       return {};
     }
 };
