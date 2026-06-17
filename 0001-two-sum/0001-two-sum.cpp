ccad20004de4705671575d46629f1a2eccc195ad
Rohan Kandpal <83447838+Rohan2605@users.noreply.github.com>
2026-06-17 10:08:10 +0530

    Time: 2 ms (72.3%), Space: 15 MB (18.75%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,33 +1,13 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        // unordered_map<int, int> mp;
+        map<int, int> mp;
+        int n = nums.size();
 
-        // for(int i=0; i<nums.size(); i++){
-        //     mp[nums[i]] = i;
-        // }
-
-        // for(int i=0; i<nums.size(); i++){
-        //     int complement = target - nums[i];
-        //     if(mp.count(complement )  && mp[complement]!=i) return {i, mp[complement]};
-        // }
-        // return {};
-
-       vector<pair<int, int>> arr;
-
-       for(int i=0; i<nums.size(); i++){
-            arr.push_back({nums[i], i});
-       }
-
-       sort(arr.begin(), arr.end());
-
-       int left=0, right=arr.size()-1;
-       while(left<right){
-            int sum = arr[left].first + arr[right].first;
-            if(sum==target) return {arr[left].second, arr[right].second};
-            else if(sum>target) right--;
-            else left++;
-       }
-       return {};
+        for(int i=0; i<n; i++){
+            if(mp.find(target - nums[i]) != mp.end()) return {mp[target-nums[i]], i};
+            mp[nums[i]] = i;
+        }
+        return {};
     }
 };
