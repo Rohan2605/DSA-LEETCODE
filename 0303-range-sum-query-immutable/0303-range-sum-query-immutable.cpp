2c01969d63611d72b6e9fe0b183a9c2fc3e3dfb5
Rohan Kandpal <83447838+Rohan2605@users.noreply.github.com>
2026-06-18 11:21:06 +0530

    Time: 4 ms (30.79%), Space: 23.9 MB (61.93%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
@@ -1,16 +1,34 @@
 class NumArray {
     vector<int> arr;
+    vector<int> prefixSum;
 public:
     NumArray(vector<int>& nums) {
         arr = nums;
+        int runSum = 0;
+        for(int i=0; i<nums.size(); i++){
+            runSum += nums[i];
+            prefixSum.push_back(runSum);
+        }
     }
     
     int sumRange(int left, int right) {
+
+        int ans = 0;
+        if((left-1)>=0) ans = prefixSum[right] - prefixSum[left-1];
+        else ans = prefixSum[right];
+        return ans;
+
         // int sum=0;
         // for(int i=left; i<=right; i++){
         //     sum += arr[i];
         // }
-        return accumulate(arr.begin()+left, arr.begin()+right+1, 0);
+
+
+        // right + 1 to define the range till right (as this type of functions don't include rightmost values)
+        // return accumulate(arr.begin()+left, arr.begin()+right+1, 0); 
+
+
+
     }
 };
 
