93f1e3d17dd782504ed10023f4533ee866ae6d39
Rohan Kandpal <83447838+Rohan2605@users.noreply.github.com>
2026-06-21 10:32:32 +0530

    Time: 5 ms (79.81%), Space: 22.8 MB (78.64%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -15,10 +15,10 @@ public:
         int low = 1;
         int high = *max_element(piles.begin(), piles.end());
 
-        while(low <= high){
+        while(low < high){
             int mid = low + (high - low)/2;
             if(canEatInTime(piles, h, mid)){
-                high = mid - 1;
+                high = mid ;
             }
             else low = mid + 1;
         }
