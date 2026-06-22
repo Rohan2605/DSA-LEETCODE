29070eff5524e0ef4e8fa864b1de90e4bdcba8f4
Rohan Kandpal <83447838+Rohan2605@users.noreply.github.com>
2026-06-22 23:57:37 +0530

    Time: 235 ms (9.76%), Space: 50.7 MB (9.11%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+
+    bool isFreqSame(vector<int> freq, vector<int> windowFreq ){
+        for(int i=0; i<26; i++){
+            if(freq[i] != windowFreq[i]) return false;
+        }
+        return true;
+    }
+
+    bool checkInclusion(string s1, string s2) {
+        vector<int> freq(26,0);
+
+        for(int i=0; i<s1.size(); i++){
+            freq[s1[i]-'a']++;
+        }
+
+        int windowSize = s1.size();
+
+        for(int i=0; i<s2.size(); i++){
+            int windowIdx = 0, idx = i;
+            vector<int> windowFreq(26,0);
+
+            while(windowIdx<windowSize && idx<s2.size()){
+                windowFreq[s2[idx]-'a']++;
+                windowIdx++; idx++;
+            }
+
+            if(isFreqSame(freq, windowFreq)) return true;
+        }
+        return false;
+    }
+};
