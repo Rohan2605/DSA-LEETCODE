650cddd0a0eee504a381df1538c77bcd89bc67a4
Rohan Kandpal <83447838+Rohan2605@users.noreply.github.com>
2026-06-23 00:13:33 +0530

    Time: 3 ms (35.63%), Space: 9.9 MB (88.09%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -13,18 +13,41 @@ public:
         // s2.pop_back();
         // return s2;
 
-        string ans, word;
-        stringstream ss(s);
+        // string ans, word;
+        // stringstream ss(s);
 
-        //Time complexity is O(n^2) 
-        while(ss>>word){
-            ans = word + " " +ans ; //because of this line which does this every time
+        // //Time complexity is O(n^2) 
+        // while(ss>>word){
+        //     ans = word + " " +ans ; //because of this line which does this every time
+        // }
+
+        // /* we can create a vector and push all the words in it  and then iterate it backwards O       (n)*/
+        
+        // ans.pop_back();
+        // return ans;
+
+        
+        reverse(s.begin(), s.end());
+        cout<<s;
+
+        string ans = "";
+        for(int i=0; i<s.size(); i++){
+            string word="";
+            while(!isspace(s[i]) && i<s.size()){
+                word += s[i];
+                i++;
+            }
+            // cout<<word;
+            if(word.size()>0) {
+                reverse(word.begin(), word.end());
+                ans += word;
+                ans += " ";
+            }
         }
 
-        /* we can create a vector and push all the words in it  and then iterate it backwards O       (n)*/
-        
         ans.pop_back();
-        return ans;
+
+        return ans; 
 
     }
 };
