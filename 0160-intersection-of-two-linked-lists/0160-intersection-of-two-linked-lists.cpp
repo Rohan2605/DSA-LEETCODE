30e23295afa21bad0335015e237722f20d07d0dc
Rohan Kandpal <83447838+Rohan2605@users.noreply.github.com>
2026-06-23 20:22:57 +0530

    Time: 79 ms (13.43%), Space: 28.8 MB (13.46%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -9,37 +9,15 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode *tempA = headA, *tempB = headB;
+        unordered_set<ListNode*> st;
 
-        int sizeA=0;
-        while(tempA){
-            sizeA++;
-            tempA = tempA -> next;
-        }
-
-        int sizeB=0;
-        while(tempB){
-            sizeB++;
-            tempB = tempB -> next;
-        }
-
-        int diff = abs(sizeA - sizeB);
-        int size = (sizeA>sizeB)? sizeB:sizeA;
-
-        if(sizeA>sizeB){
-            for(int i=0; i<diff; i++){
-                headA = headA->next;
-            }
-        }
-        else{
-            for(int i=0; i<diff; i++){
-                headB = headB->next;
-            }
-        }
-
-        for(int i=0; i<size; i++){
-            if(headA == headB) return headA;
+        while(headA){
+            st.insert(headA);
             headA = headA->next;
+        }
+
+        while(headB){
+            if(st.count(headB)) return headB;
             headB = headB->next;
         }
 
