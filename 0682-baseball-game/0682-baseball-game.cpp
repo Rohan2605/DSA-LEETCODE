dd2adbcbd138f90048935fcbc943dc8deecbf6af
Rohan Kandpal <83447838+Rohan2605@users.noreply.github.com>
2026-06-29 19:12:34 +0530

    Time: 11 ms (1.7%), Space: 11.9 MB (81.38%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.cpp
+++ 0682-baseball-game/0682-baseball-game.cpp
@@ -4,16 +4,13 @@ public:
         vector<int> ops;
 
         for(int i=0; i<op.size(); i++){
-            if (isdigit(op[i][0]) || op[i][0] == '-') ops.push_back(stoi(op[i]));            
+            if (isdigit(op[i][0]) || op[i][0] == '-'){
+                ops.push_back(stoi(op[i])); 
+            }            
             else if(op[i] == "C") ops.pop_back();
             else if(op[i] == "D") ops.push_back(2 * (ops.back()));
             else if(op[i] == "+") {
-                int x = (ops.back());
-                ops.pop_back();
-                int y = (ops.back());
-                int z = x + y;
-                ops.push_back(x);
-                ops.push_back(z);
+                ops.push_back(ops.back() + ops[ops.size()-2]);
             }
         }
 
