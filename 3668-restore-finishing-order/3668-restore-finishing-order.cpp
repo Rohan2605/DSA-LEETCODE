11e4e516945fbb5c6aac7b5c61dd7f2a072f9ce8
Rohan Kandpal <83447838+Rohan2605@users.noreply.github.com>
2026-06-30 17:38:17 +0530

    Time: 0 ms (100%), Space: 50 MB (99.4%) - LeetHub

---
+++ 3668-restore-finishing-order/3668-restore-finishing-order.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> recoverOrder(vector<int>& order, vector<int>& friends) {
+        vector<int> ans;
+
+        for(int x: order){
+            if(binary_search(friends.begin(), friends.end(), x)) ans.push_back(x);
+        }
+        return ans;
+
+    }
+};
