136509c6c148474e87d51dc2b0940717330e7722
Rohan Kandpal <83447838+Rohan2605@users.noreply.github.com>
2026-07-05 11:41:27 +0530

    Time: 0 ms (100%), Space: 24.4 MB (71.8%) - LeetHub

---
+++ 3688-bitwise-or-of-even-numbers-in-an-array/3688-bitwise-or-of-even-numbers-in-an-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int evenNumberBitwiseORs(vector<int>& nums) {
+        int sum = 0;
+        
+        for(int x: nums){
+            if(x % 2 == 0) sum |= x;
+        }
+
+        return sum;
+    }
+};
